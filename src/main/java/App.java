959e1ef45b6ca065c366441a962af6236a245f25
Dun-Njuguna <duncankariuki60@gmail.com>
2019-02-18 14:36:45 +0300

    modified ProcessBuilder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,20 +5,18 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-
-
-    ProcessBuilder process = new ProcessBuilder(); 
-    Integer port;
-    // This tells our app that if Heroku sets a port for us, we need to use that port.
-    // Otherwise, if they do not, continue using port 4567.
-    if (process.environment().get("PORT") != null) {
-        port = Integer.parseInt(process.environment().get("PORT"));
-    } else {
-        port = 4567;
-    }
-    setPort(port);
-
     public static void main(String[] args) {
+
+        ProcessBuilder process = new ProcessBuilder();
+         Integer port;
+         if (process.environment().get("PORT") != null) {
+             port = Integer.parseInt(process.environment().get("PORT"));
+         } else {
+             port = 4567;
+         }
+    
+        setPort(port);
+
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 

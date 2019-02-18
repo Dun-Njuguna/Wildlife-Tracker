032d5611ad82809caa6342820d8db9009608810f
Dun-Njuguna <duncankariuki60@gmail.com>
2019-02-18 14:13:39 +0300

    updated build.gradle

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,15 +7,15 @@ import static spark.Spark.*;
 public class App {
 
 
-    ProcessBuilder process = new ProcessBuilder();
-    Integer port;
-    if (process.environment().get("PORT") != null) {
-        port = Integer.parseInt(process.environment().get("PORT"));
-    } else {
-        port = 4567;
-    }
+//     ProcessBuilder process = new ProcessBuilder();
+//     Integer port;
+//     if (process.environment().get("PORT") != null) {
+//         port = Integer.parseInt(process.environment().get("PORT"));
+//     } else {
+//         port = 4567;
+//     }
 
-   setPort(port);
+//    setPort(port);
 
     public static void main(String[] args) {
         staticFileLocation("/public");
